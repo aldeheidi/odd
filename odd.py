52afb2fc7e182a1284440b095a1f176619ca164e
aldeheidi <heidi.aleong@gmail.com>
2023-07-06 10:18:06 -0400

    first commit

---
+++ odd.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  6 09:17:30 2023
+
+@author: Heidi
+"""
+import sys
+
+x = sys.argv[1]
+n = int(x)
+
+if n%2==1:
+    print('odd')
+else:
+    print('even')
